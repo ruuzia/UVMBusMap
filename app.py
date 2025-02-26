442fd17f97eb403f195e84b53b7f386e65aaee52
Rustum Zia <ziarustum@gmail.com>
2025-02-26 14:41:44 -0500

    Fetch cat buses from new API url

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ GMT_ARRIVALS_URL = "https://maps.trilliumtransit.com/gtfsmap-realtime/feed/ccta-
 GMT_ROUTES_URL = "https://gtfs-api.trilliumtransit.com/gtfs-api/routes/by-feed/ccta-vt-us"
 GMT_STOPS_URL = "https://gtfs-api.trilliumtransit.com/gtfs-api/stops/by-feed/ccta-vt-us/route-id/19137,19139,13496,13497,13498,3190,3191,7458,11182,7455,74419,74409,74410,74411,19141,74418,74413,3175,74412,3153,3176,3167,3177,3181,3168,3183,3197,3194,19140,3195,3196,3186,7335,74414,74415,3171,3172,3174,3188,74416,74417,3163,3164,3165,19138,32941,32942,11183,19143,19142,19144,19145,"
 STOP_DATA = "static/routes.json"
+CATS_URL_NEW = "https://api.peaktransit.com/v5/index.php?app_id=_RIDER&key=c620b8fe5fdbd6107da8c8381f4345b4&controller=vehicles2&action=list&agencyID=159&routeID=12361"
 
 DAVIS_CENTER_GMT_STOPCODE = "805489"
 UHEIGHT_STOPCODE = "805490"
@@ -50,15 +51,15 @@ def gmt_buses():
 
 
 def cat_buses():
-    res = requests.get(CAT_VEHICLES_URL)
+    res = requests.get(CATS_URL_NEW)
     data = res.json()
     if data['success'] != True:
         print("ERROR fetching cat buses")
         return None
 
-    return [{'id': bus['id'],
-             'lat': bus['position'][0],
-             'lon': bus['position'][1]} for bus in data['vehicles']]
+    return [{'id': bus['vehicleName'],
+             'lat': bus['lat'],
+             'lon': bus['lng']} for bus in data['vehicles']]
 
 def vehicles():
     return {
